d1b0a5602767f9b38988298774f43ad9cfc5590f
apelserg <apelserg@mail.ru>
2015-05-14 17:57:42 +0300

    new

---
+++ html-start-page-on-canvas-applebloom-canva.js
@@ -0,0 +1,61 @@
+﻿"use strict";
+
+//===
+// Отрисовка на канве
+//===
+APELSERG.CANVA.Rewrite = function () {
+
+    var ctx = APELSERG.CONFIG.PROC.Ctx;
+
+    // фон
+    ctx.drawImage(APELSERG.CONFIG.PROC.Img, 0, 0, APELSERG.CONFIG.PROC.CanvaID.width, APELSERG.CONFIG.PROC.CanvaID.height);
+
+    // часы
+    var dateCurrent = new Date();
+    var hours = dateCurrent.getHours();
+    var hoursStr =  hours < 10 ? "0" + hours.toString() : hours.toString();
+    var minutes = dateCurrent.getMinutes();
+    var minutesStr =  minutes < 10 ? "0" + minutes.toString() : minutes.toString();
+    var seconds = dateCurrent.getSeconds();
+    var secondsStr =  seconds < 10 ? "0" + seconds.toString() : seconds.toString();
+
+    var dateCurrentStr = hoursStr  + ":" + minutesStr + ":" + secondsStr;
+
+    ctx.font = "30px Arial";
+    ctx.textAlign = "left";
+    ctx.strokeStyle = "blue";
+    ctx.strokeText(dateCurrentStr, 20, 30);
+
+    // ссылки
+    for (var n = 0 in APELSERG.MODEL.DATA.Links) {
+
+        var link = APELSERG.MODEL.DATA.Links[n];
+
+        if (link.ShowBorder) {
+            if (link.SelectCnt == 0) ctx.strokeStyle = link.Color;
+            else ctx.strokeStyle = link.SelectColor; 
+            ctx.strokeRect(link.X, link.Y, link.LengthX, link.LengthY);
+        }
+
+        ctx.font = link.FontHeight.toString() + "px Arial";
+        ctx.textAlign = "left";
+
+        if (link.SelectName) ctx.fillStyle = link.SelectColor;
+        else ctx.fillStyle = link.Color;
+
+        ctx.fillText(link.Name, link.X + 5, link.Y + link.FontHeight);
+    }
+
+    // лепестки (поверх контента)
+    for (var n = 0 in APELSERG.MODEL.DATA.Applebloom) {
+
+        var flake = APELSERG.MODEL.DATA.Applebloom[n];
+
+        ctx.beginPath();
+        ctx.arc(flake.X - flake.DeltaX, flake.Y, flake.Size / 2 - flake.DeltaR, 0, flake.FlipSize * Math.PI, flake.FlipRotate);
+        ctx.arc(flake.X, flake.Y, flake.Size / 2, 0, flake.FlipSize * Math.PI, flake.FlipRotate);
+        ctx.arc(flake.X + flake.DeltaX, flake.Y, flake.Size / 2 - flake.DeltaR, 0, flake.FlipSize * Math.PI, flake.FlipRotate);
+        ctx.fillStyle = flake.Color;
+        ctx.fill();
+    }
+}
---
+++ html-start-page-on-canvas-applebloom-model.js
@@ -0,0 +1,203 @@
+﻿"use strict";
+
+//===
+// Ссылка
+//===
+APELSERG.MODEL.Link = function (linkUrl, linkName, linkX, linkY, lengthX, lengthY, linkColor) {
+    this.Url = linkUrl;
+    this.Name = linkName;
+    this.X = linkX;
+    this.Y = linkY;
+    this.LengthX = lengthX;
+    this.LengthY = lengthY;
+    this.Color = linkColor;
+    this.SelectColor = 'orange';
+    this.SelectCnt = 0;
+    this.SelectName = false;
+    this.ShowBorder = false;
+    this.FontHeight = 20;
+}
+
+//===
+// Массив ссылок
+//===
+APELSERG.MODEL.MakeLinks = function () {
+
+    var baseX = APELSERG.CONFIG.SET.BaseLinkX;
+    var baseY = APELSERG.CONFIG.SET.BaseLinkY + 50;
+
+    var links = [];
+
+    var linksList = [
+        { name: "GitHub - Шаблонетка Start page Applebloom", url: "https://github.com/apelserg/stencil-html-start-page-on-canvas-applebloom" },
+    ];
+
+    for (var n = 0 in linksList) {
+
+        var color = "blue";
+        var link = new APELSERG.MODEL.Link(linksList[n].url, linksList[n].name, baseX, baseY + 30 * n, linksList[n].name.length * 10 + 30, 30, color);
+
+        links.push(link);
+    }
+    return links;
+}
+
+
+
+//===
+// Лепесток
+//===
+APELSERG.MODEL.Flake = function (flakeX, flakeY, flakeSize, shiftDir, flipSize, flipDir, flakeColor) {
+    this.BaseX = flakeX;    
+    this.X = flakeX;
+    this.Y = flakeY;
+    this.Size = flakeSize;
+    this.ShiftDir = shiftDir;
+    this.FlipSize = flipSize;
+    this.FlipDir = flipDir;
+    this.FlipRotate = true;
+    this.DeltaX = 3;
+    this.DeltaR = 2;
+    this.CntFlip = 0;
+    this.CntRotate = 0;
+    this.Color = flakeColor;
+}
+
+//===
+// Массив лепестков
+//===
+APELSERG.MODEL.MakeApplebloom = function (flakeNum) {
+
+    var Applebloom = [];
+    var color = "white";
+
+    for (var n = 0; n < flakeNum; n++) {
+
+        var shiftDir = 1;
+        if (Math.round(Math.random() * 100) % 2 == 0) shiftDir = -1;
+
+        var flipDir = 1;
+        if (Math.round(Math.random() * 100) % 2 == 0) {
+            flipDir = -1;
+            color = "#FFD8FF";
+        }
+        else {
+            color = "#FFF0FF";
+        }
+
+        var x = Math.round(Math.random() * APELSERG.CONFIG.SET.PicWidth);
+        var y = Math.round(Math.random() * APELSERG.CONFIG.SET.PicHeight);
+
+        var flipSize = (Math.random() * 100) % 2.0;
+
+        var flake = new APELSERG.MODEL.Flake(x, y, APELSERG.CONFIG.SET.ApplebloomSize, shiftDir, flipSize, flipDir, color);
+
+        Applebloom.push(flake);
+    }
+    return Applebloom;
+}
+
+//===
+// Проверка клика на кнопке
+//===
+APELSERG.MODEL.CheckClickFrame = function (frame) {
+
+    if ((APELSERG.CONFIG.PROC.MouseClickX > frame.X)
+        && (APELSERG.CONFIG.PROC.MouseClickX < frame.X + frame.LengthX)
+        && (APELSERG.CONFIG.PROC.MouseClickY > frame.Y)
+        && (APELSERG.CONFIG.PROC.MouseClickY < frame.Y + frame.LengthY)){
+
+        return true;
+    }
+    return false;
+}
+
+//===
+// Проверка мыши над кнопкой
+//===
+APELSERG.MODEL.CheckMoveFrame = function (frame) {
+
+    if ((APELSERG.CONFIG.PROC.MouseMoveX > frame.X)
+        && (APELSERG.CONFIG.PROC.MouseMoveX < frame.X + frame.LengthX)
+        && (APELSERG.CONFIG.PROC.MouseMoveY > frame.Y)
+        && (APELSERG.CONFIG.PROC.MouseMoveY < frame.Y + frame.LengthY)) {
+
+        return true;
+    }
+    return false;
+}
+
+//===
+// Нажатие кнопок
+//===
+APELSERG.MODEL.UpdateButtons = function () {
+
+    for (var n = 0 in APELSERG.MODEL.DATA.Links) {
+
+        var link = APELSERG.MODEL.DATA.Links[n];
+
+        link.SelectName = APELSERG.MODEL.CheckMoveFrame(link);
+        if (link.SelectCnt > 0) link.SelectCnt--;
+
+        if (APELSERG.MODEL.CheckClickFrame(link)) {
+
+            link.SelectCnt = APELSERG.CONFIG.SET.CntSelect;
+
+            window.open(link.Url, "_blank");
+        }
+    }
+
+    APELSERG.CONFIG.PROC.MouseClickX = -999;
+    APELSERG.CONFIG.PROC.MouseClickY = -999;
+
+    //APELSERG.CONFIG.PROC.MouseMoveX = -999;
+    //APELSERG.CONFIG.PROC.MouseMoveY = -999;
+
+}
+//===
+// Переместить лепестки
+//===
+APELSERG.MODEL.UpdateApplebloom = function () {
+
+    for (var n = 0 in APELSERG.MODEL.DATA.Applebloom) {
+
+        var flake = APELSERG.MODEL.DATA.Applebloom[n];
+
+        var dir = 1;
+        if (Math.round(Math.random() * 100) % 2 == 0) dir = -1;
+
+        if (Math.abs(flake.BaseX - flake.X) > 200) {
+            flake.ShiftDir *= -1;
+        }
+        flake.X += flake.ShiftDir * Math.round(Math.random() * 100) % 3;
+        
+        flake.Y += Math.round(Math.random() * 100) % 3;
+        if (flake.Y > APELSERG.CONFIG.SET.PicHeight) flake.Y = 1;
+
+        flake.CntFlip--;
+        if (flake.CntFlip <= 0) {
+
+            flake.CntFlip = Math.round(Math.random() * 5);
+
+            flake.FlipSize += 0.05 * flake.FlipDir + 0.05 * dir;
+            if (flake.FlipSize > 1.3) {
+                flake.FlipSize = 1.3;
+                flake.FlipDir *= -1;
+            }
+            if (flake.FlipSize < 0.7) {
+                flake.FlipSize = 0.7;
+                flake.FlipDir *= -1;
+            }
+        }
+
+        flake.CntRotate--;
+        if (flake.CntRotate <= 0) {
+
+            flake.CntRotate = Math.round(Math.random() * 100);
+
+            flake.DeltaX = Math.round(Math.random() * 100) % 7;
+
+            if (dir > 0 && (flake.FlipSize <= 1.1 && flake.FlipSize >= 0.9)) flake.FlipRotate = !flake.FlipRotate;
+        }
+    }
+}
